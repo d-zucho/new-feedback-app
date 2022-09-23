a80c02083ecdc12026198de51e22d3728bef0b42
d-zucho <danny.nico.dev@outlook.com>
2022-09-23 11:28:53 -0700

    try to incorporate framer-motion for smoother enter and exit animations of review items

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,17 +1,39 @@
 import FeedbackItem from './FeedbackItem'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const Reviews = ({ feedbackData, handleDelete }) => {
   return (
     <section>
-      {feedbackData.map((review) => (
-        <FeedbackItem
-          key={review.id}
-          review={review}
-          handleDelete={handleDelete}
-        />
-      ))}
+      <AnimatePresence>
+        {feedbackData.map((review) => (
+          <motion.div
+            key={review.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={review.id}
+              review={review}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </section>
   )
+
+  // return (
+  //   <section>
+  //     {feedbackData.map((review) => (
+  //       <FeedbackItem
+  //         key={review.id}
+  //         review={review}
+  //         handleDelete={handleDelete}
+  //       />
+  //     ))}
+  //   </section>
+  // )
 }
 
 export default Reviews
